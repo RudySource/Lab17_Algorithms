79c2e103198b878716711f4998fae5eb9679dc29
Rud3us <rodionpylnev@yandex.ru>
2026-03-16 19:56:15 +0300

    Step 8: functions and modular programming

---
+++ code/Step7_1/Program.cs
@@ -0,0 +1,5 @@
+﻿int[] m = { 1, 2, 3 };
+
+for (int i = 0; i < m.Length; i++) {
+    Console.WriteLine($"Эл - {i}: {m[i]}");
+}
---
+++ code/Step8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Калькулятор с функциями\nВведите первое число: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"Сложение: {num1} + {num2} = {Sum(num1, num2)}\nВычитание: {num1} - {num2} = {Subtract(num1, num2)}\nУмножение: {num1} * {num2} = {Multiply(num1, num2)}\nДеление: {num1} / {num2} = {Divide(num1, num2)}");
+static double Sum(double a, double b) => a + b;
+static double Subtract(double a, double b) => a - b;
+static double Multiply(double a, double b) => a * b;
+static double Divide(double a, double b) => a / b;
